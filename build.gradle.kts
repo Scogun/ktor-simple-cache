13a8de4bf693cd723ff11ddd76dde4c7d262c075
Sergey <AntonovSergeyS@gmail.com>
2023-12-03 12:54:07 +0100

    Fix versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@ tasks.wrapper {
     gradleVersion = "8.5"
 }
 
-
 allprojects {
+
+    version = "0.0.1"
+
     repositories {
         mavenCentral()
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "simple-cache"
+rootProject.name = "com.ucasoft.ktor"
 
 include("ktor-simple-cache")
 
