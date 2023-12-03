f96113cb5a8640aca0b4365ab97100112b8d0e06
Sergey <AntonovSergeyS@gmail.com>
2023-12-03 12:54:32 +0100

    Add publication tasks

--- ktor-simple-redis-cache/build.gradle.kts
+++ ktor-simple-redis-cache/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.serialization") version "1.9.21"
+    `maven-publish`
 }
 
 dependencies {
@@ -23,3 +24,32 @@ kotlin {
     jvmToolchain(11)
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("Simple-Redis-Cache") {
+            pom {
+                name.set("Ktor Simple Redis Cache")
+                description.set("Redis cache provider for Simple Cache plugin")
+                url.set("https://github.com/Scogun/ktor-simple-cache")
+                licenses {
+                    license {
+                        name.set("GPL-3.0 License")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Scogun")
+                        name.set("Sergey Antonov")
+                        email.set("SAntonov@ucasoft.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Scogun/ktor-simple-cache.git")
+                    developerConnection.set("scm:git:ssh://github.com:Scogun/ktor-simple-cache.git")
+                    url.set("https://github.com/Scogun/ktor-simple-cache")
+                }
+            }
+        }
+    }
+}
