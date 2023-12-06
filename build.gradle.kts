3e50597ec7c9ff4fa00e30039b786864cce4c6ed
Sergey Antonov <AntonovSergeyS@gmail.com>
2023-12-07 00:39:57 +0100

    Add Tests reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("jacoco-report-aggregation")
+}
+
 tasks.wrapper {
     gradleVersion = "8.5"
 }
@@ -9,6 +13,7 @@ allprojects {
     version = "0.0.1"
 
     apply {
+        plugin("jacoco")
     }
 
     repositories {
@@ -17,5 +22,36 @@ allprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+        reports {
+            junitXml.required.set(true)
+        }
+    }
+
+    jacoco {
+        toolVersion = "0.8.9"
+    }
+}
+
+tasks.register<Copy>("collectXmlReports") {
+    val testTasks = rootProject.subprojects.flatMap { it.tasks.withType<Test>() }
+    val locations = testTasks.map { it.reports.junitXml.outputLocation.asFile.get() }
+    from(*locations.toTypedArray())
+    into(rootProject.layout.buildDirectory.dir("test-results/test"))
+    include("*.xml")
+
+    mustRunAfter(*testTasks.toTypedArray())
+}
+
+dependencies {
+    jacocoAggregation(project(":ktor-simple-cache"))
+    jacocoAggregation(project(":ktor-simple-memory-cache"))
+    jacocoAggregation(project(":ktor-simple-redis-cache"))
+}
+
+reporting {
+    reports {
+        val codeCoverageReport by creating(JacocoCoverageReport::class) {
+            testType.set(TestSuiteType.UNIT_TEST)
+        }
     }
 }
