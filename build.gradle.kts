b97ec288e3533268c72220b1b253281b7164f794
Sergey <AntonovSergeyS@gmail.com>
2023-12-10 10:57:10 +0100

    Feature/test reports (#2)
    * Add Tests reporting
    
    * Add Test action
    
    * Add Jacoco
    
    * Fix
    
    * Update JaCoCo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("jacoco-report-aggregation")
+}
+
 tasks.wrapper {
     gradleVersion = "8.5"
 }
@@ -9,6 +13,7 @@ allprojects {
     version = "0.0.1"
 
     apply {
+        plugin("jacoco")
     }
 
     repositories {
@@ -17,5 +22,26 @@ allprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+        reports {
+            junitXml.required.set(true)
+        }
+    }
+
+    jacoco {
+        toolVersion = "0.8.11"
+    }
+}
+
+dependencies {
+    jacocoAggregation(project(":ktor-simple-cache"))
+    jacocoAggregation(project(":ktor-simple-memory-cache"))
+    jacocoAggregation(project(":ktor-simple-redis-cache"))
+}
+
+reporting {
+    reports {
+        val codeCoverageReport by creating(JacocoCoverageReport::class) {
+            testType.set(TestSuiteType.UNIT_TEST)
+        }
     }
 }
