dd025ac625feca53133587e5202b494815c2c233
Sergey <AntonovSergeyS@gmail.com>
2024-02-09 10:43:22 +0100

    Update Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     resolutionStrategy {
         plugins {
-            val kotlinVersion = "1.9.21"
+            val kotlinVersion = "1.9.22"
             kotlin("multiplatform") version kotlinVersion
             kotlin("plugin.serialization") version kotlinVersion
         }
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "simple-cache"
