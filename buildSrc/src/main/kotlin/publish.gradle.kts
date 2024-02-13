68f67696982e08a7cbaaa962efe84fd3536ee835
Sergey <AntonovSergeyS@gmail.com>
2024-02-13 19:49:49 +0100

    Fix publishing

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -55,3 +55,8 @@ publishing {
 signing {
     sign(publishing.publications)
 }
+
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    val signingTasks = tasks.withType<Sign>()
+    mustRunAfter(signingTasks)
+}
