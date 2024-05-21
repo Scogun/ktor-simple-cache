36e1cb57ce1344a693bb011f5638ae00c065cb5f
Sergey <AntonovSergeyS@gmail.com>
2024-05-21 21:14:46 +0200

    Update libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
             val kotlinVersion = "1.9.24"
             kotlin("multiplatform") version kotlinVersion apply false
             kotlin("plugin.serialization") version kotlinVersion apply false
-            id("org.jetbrains.kotlinx.kover") version "0.7.6" apply false
+            id("org.jetbrains.kotlinx.kover") version "0.8.0" apply false
         }
     }
 }
