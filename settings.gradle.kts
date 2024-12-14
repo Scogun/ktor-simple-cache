56a1673f628a114a13a43f7ce71750024a3f5c83
Sergey <AntonovSergeyS@gmail.com>
2024-12-14 10:36:43 +0100

    Update Plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,13 @@ pluginManagement {
             val kotlinVersion = "2.0.21"
             kotlin("multiplatform") version kotlinVersion apply false
             kotlin("plugin.serialization") version kotlinVersion apply false
-            id("org.jetbrains.kotlinx.kover") version "0.8.3" apply false
+            id("org.jetbrains.kotlinx.kover") version "0.9.0" apply false
         }
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 rootProject.name = "simple-cache"
