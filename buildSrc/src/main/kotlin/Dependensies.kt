ce564f53e94b29df572b59a8d17556362d37fca9
Sergey <AntonovSergeyS@gmail.com>
2024-12-14 10:36:21 +0100

    Update libraries

--- buildSrc/src/main/kotlin/Dependensies.kt
+++ buildSrc/src/main/kotlin/Dependensies.kt
@@ -1,6 +1,6 @@
 import org.gradle.api.Project
 
-const val ktorVersion = "3.0.1"
+const val ktorVersion = "3.0.2"
 const val kotestVersion = "5.9.1"
 
 fun Project.ktor(module: String) = "io.ktor:ktor-$module:$ktorVersion"
