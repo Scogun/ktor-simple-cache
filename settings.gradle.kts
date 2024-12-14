2a91460a61e92896a688ff83511caa34ae1ae101
Sergey <AntonovSergeyS@gmail.com>
2024-12-14 10:36:53 +0100

    Update Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     resolutionStrategy {
         plugins {
-            val kotlinVersion = "2.0.21"
+            val kotlinVersion = "2.1.0"
             kotlin("multiplatform") version kotlinVersion apply false
             kotlin("plugin.serialization") version kotlinVersion apply false
             id("org.jetbrains.kotlinx.kover") version "0.9.0" apply false
