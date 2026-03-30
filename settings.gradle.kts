f2e92a34125f723cad41be453854ef1d6f4a04c3
Sergey <AntonovSergeyS@gmail.com>
2026-03-30 20:23:17 +0200

    Update Plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
             val kotlinVersion = "2.3.20"
             kotlin("multiplatform") version kotlinVersion apply false
             kotlin("plugin.serialization") version kotlinVersion apply false
-            id("org.jetbrains.kotlinx.kover") version "0.9.7" apply false
+            id("org.jetbrains.kotlinx.kover") version "0.9.8" apply false
             id("com.vanniktech.maven.publish") version "0.36.0" apply false
         }
     }
